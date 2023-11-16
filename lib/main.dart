0ceb436c95ca428d029b9778de88463a97cbc4f6
davids <148128332+davidHubs1@users.noreply.github.com>
2023-11-16 15:13:13 -0500

    customm

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      title: 'CinemaPedia',
+      title: 'CinemaPedia - Flutter',
       routerConfig: router,
     );
   }
